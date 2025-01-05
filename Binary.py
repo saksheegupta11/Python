93927300c32999b96508fd875491426d743a3fcb
saksheegupta11 <saksheegupta520@gmail.com>
2025-01-06 00:32:29 +0530

    Add files via upload

---
+++ Binary.py
@@ -0,0 +1,5 @@
+x=15
+print(x, '-', bin(x))
+
+y=0b11000
+print(bin(y), '-', int(y))
---
+++ Keywords.py
@@ -0,0 +1,8 @@
+import keyword
+# keywords in python
+print(keyword.kwlist)
+print(len(keyword.kwlist))
+
+# softkeywords in python
+print(keyword.softkwlist)
+print(len(keyword.softkwlist))
